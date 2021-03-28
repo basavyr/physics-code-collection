83a7245ec23feb171b64921b3a46b5a200a4e873
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-03-28 08:44:42 +0300

    adds Pauli Matrix and matrix power function into the module

--- Code-Collection/Clebsch-Gordan-Coeffs/CG-Series/wignerD.py
+++ Code-Collection/Clebsch-Gordan-Coeffs/CG-Series/wignerD.py
@@ -2,15 +2,24 @@
 
 import numpy as np
 
-import matplotlib.pyplot as plt
-
-
+# import matplotlib.pyplot as plt
+from numpy.linalg import matrix_power
 import gordan as CGC
 
 # define the imaginary unit
 I = 1j
 
+
+# define a function for raising a matrix to a power
+def Matrix_Power(mat, n):
+    mat_n = matrix_power(mat, n)
+    return mat_n
+
+
 # define the Pauli matrix for a rotation around the Y-axis
 Sigma_Y = [[0, -I], [I, 0]]
 
-print(np.array(Sigma_Y))
+for id in range(5):
+    print(f'n={id+1}')
+    sy_2n = Matrix_Power(Sigma_Y, 2 * id + 1)
+    print(np.array(sy_2n))
