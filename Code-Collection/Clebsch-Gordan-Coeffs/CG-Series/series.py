7ce114ce6741586fb25d54e21460736531ace941
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-03-29 09:45:36 +0300

    the CG series has improved output now. failsafe introduced in the CG-series method

--- Code-Collection/Clebsch-Gordan-Coeffs/CG-Series/series.py
+++ Code-Collection/Clebsch-Gordan-Coeffs/CG-Series/series.py
@@ -25,30 +25,44 @@ def reducedSeries(j1, j2):
     m1Values = makeM(j1)
     m2Values = makeM(j2)
     beta = 25 * np.pi / 180.0
+    CGC_products = []
+    Wd_products = []
+    OK_Status = 1
     with open(coeffs, 'w+') as filer:
         for m1P in m1Values:
-            # filer.write('new m1`\n')
             for m2P in m2Values:
-                # filer.write('new m2`\n')
                 for m1 in m1Values:
                     for m2 in m2Values:
                         m_index = f'm1`={m1P} m1={m1} ' + f'm2`={m2P} m2={m2}'
-                        CG01 = cg_half(m1P, m2P)
-                        CG02 = cg_half(m1, m2)
-                        CG = CG01 * CG02
-                        wd1 = WD.Wigner_d(j1, beta, m1P, m1)
-                        wd2 = WD.Wigner_d(j2, beta, m2P, m2)
-                        WD_12 = wd1 * wd2
-                        coeff_list = str(CG01) + ' ' + str(CG02) + \
-                            ' ' + str(CG) + ' ' + str(WD_12)
-                        if(np.isnan(WD_12)):
+
+                        # compute the two Wigner-d functions within the redcued formula
+                        wd1 = WD.Wigner_d(j1, beta, m1P, m1, 0)
+                        wd2 = WD.Wigner_d(j2, beta, m2P, m2, 0)
+                        Wd_12 = wd1 * wd2
+                        if(np.isnan(Wd_12)):
                             filer.writelines(m_index + '\n')
                             filer.writelines('Non-Physical solutions' + '\n')
+                            OK_Status = 0
                             return -1
-                        results = [m1P, m2P, m1, m2, CG01, CG02, wd1, wd2]
-                        if(CG != 0):
+
+                        # compute the CGC coefficients within the reduced formula
+                        CG01 = cg_half(m1, m2)
+                        CG02 = cg_half(m1P, m2P)
+                        CG_12 = float(CG01 * CG02)
+                        if(np.isnan(CG_12)):
                             filer.writelines(m_index + '\n')
-                            filer.writelines(str(results) + '\n')
+                            filer.writelines('Non-Physical solutions' + '\n')
+                            OK_Status = 0
+                            return -1
+
+                        results = [m1P, m1, m2P, m2, CG_12 * Wd_12]
+                        CGC_products.append(CG_12)
+                        Wd_products.append(Wd_12)
+    if(OK_Status):
+        print('Reduced series computation finished successfully!')
+    else:
+        print('Computation of the Clebsch-Gordan series failed!')
+    return [CGC_products, Wd_products]
 
 
-reducedSeries(j1, j2)
+CG_Series = reducedSeries(j1, j2)
