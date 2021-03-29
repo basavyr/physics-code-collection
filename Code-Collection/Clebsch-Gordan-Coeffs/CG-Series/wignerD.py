60d8663bffa2c63d40543246d822318c0e4e92eb
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-03-29 07:00:41 +0300

    adds calculation for Wigner-d matrix

--- Code-Collection/Clebsch-Gordan-Coeffs/CG-Series/wignerD.py
+++ Code-Collection/Clebsch-Gordan-Coeffs/CG-Series/wignerD.py
@@ -13,22 +13,54 @@ I = 1j
 # define a function for raising a matrix to a power
 def Matrix_Power(mat, n):
     mat_n = matrix_power(mat, n)
-    re turn mat_n
+    return mat_n
 
 
 # define the Pauli matrix for a rotation around the Y-axis
 Sigma_Y = [[0, -I], [I, 0]]
 
-for id in range(5):
-    print(f'n={id+1}')
-    sy_2n = Matrix_Power(Sigma_Y, 2 * id + 1)
-    print(np.array(sy_2n))
+# for id in range(5):
+#     print(f'n={id+1}')
+#     sy_2n = Matrix_Power(Sigma_Y, 2 * id + 1)
+#     print(np.array(sy_2n))
 
 
 def cPlus(j, m):
     t1 = j - m
     t2 = j + m + 1
-    try:
-        t = np.sqrt(t1 * t2)
-    except:
-        print('Non-physical solutions')
+    t = t1 * t2
+    if(t < 0):
+        print('Non-physical solution -> ', t1, t2)
+        return -1
+    elif (t == 0):
+        return 0
+    else:
+        return np.sqrt(t)
+
+
+def cMinus(j, m):
+    t1 = j + m
+    t2 = j - m + 1
+    t = t1 * t2
+    if(t < 0):
+        print('Non-physical solution -> ', t1, t2)
+        return -1
+    elif (t == 0):
+        return 0
+    else:
+        return np.sqrt(t)
+
+
+def Delta(a, b):
+    if(a == b):
+        return 1
+    return 0
+
+
+def Wigner_d(j, beta, m1, m2):
+    t0 = np.cos(beta / 2.0) * Delta(m1, m2)
+    t1 = cPlus(j, m2) * Delta(m1, m2 + 1)
+    t2 = cMinus(j, m2) * Delta(m1, m2 - 1)
+    t3 = np.sin(beta / 2.0) * (t1 - t2)
+    t = t0 - t3
+    return t
