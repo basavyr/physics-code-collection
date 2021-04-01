9b16c9ea990c89522c91afdeee51f167b8d79c66
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-01 13:58:36 +0300

    import the data stack properly using multi-band approach

--- Code-Collection/Energy-Staggering-Wobbling/python/staggering.py
+++ Code-Collection/Energy-Staggering-Wobbling/python/staggering.py
@@ -3,28 +3,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data_file = 'data.dat'
 
-with open(data_file, 'r+') as data:
-    lines = data.readlines()
-    data = []
-    sub_data = []
-    eol_check = len(lines)
-    count = 0
-    for line in lines:
-        x = line.strip('\n')
-        x = x.split(' ')
-        count = count + 1
-        if(len(x) == 1):
-            data.append(sub_data)
-            sub_data = []
-        elif count == eol_check:
-            data.append(sub_data)
-            sub_data = []
-        elif (len(x) == 2):
-            spin = float(x[0])
-            energy = float(x[1])
-            data_tuple = [spin, energy]
-            sub_data.append(data_tuple)
-for big_data in data:
-    print(big_data)
+DATA_STACK_1 = 'data_stack1.dat'
+DATA_STACK_2 = 'data_stack2.dat'
+
+
+def Import_Data(FILE):
+    with open(FILE, 'r+') as DATA_STACK:
+        lines = DATA_STACK.readlines()
+        DATA = []
+        sub_data = []
+        eol_check = len(lines)
+        count = 0
+        for line in lines:
+            x = line.strip('\n')
+            x = x.split(' ')
+            count = count + 1
+            # print(x, count)
+            if (len(x) == 2):
+                print('IN STACK-BUILD')
+                print(x, count)
+                spin = float(x[0])
+                energy = float(x[1])
+                data_tuple = [spin, energy]
+                sub_data.append(data_tuple)
+            if (count == eol_check and len(x) == 2):
+                print('IN EOL CHECK')
+                # print(sub_data)
+                # print(DATA)
+                DATA.append(sub_data)
+                # print(DATA)
+                sub_data = []
+            elif (len(x) == 1):
+                print('IN MULTI-BAND')
+                # print(sub_data)
+                DATA.append(sub_data)
+                # print(DATA)
+                sub_data = []
+    if(len(DATA) == 0):
+        return 'The experimental data stack is empty!'
+    else:
+        return DATA
+
+
+print(Import_Data(DATA_STACK_1))
