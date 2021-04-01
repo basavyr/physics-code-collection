0e54fdb9e0445a3b27182caeda1989fbb746b289
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-01 11:27:51 +0300

    the importer now uses the - delimiter properly. multi-band data import works

--- Code-Collection/Energy-Staggering-Wobbling/python/staggering.py
+++ Code-Collection/Energy-Staggering-Wobbling/python/staggering.py
@@ -8,13 +8,23 @@ data_file = 'data.dat'
 with open(data_file, 'r+') as data:
     lines = data.readlines()
     data = []
+    sub_data = []
+    eol_check = len(lines)
+    count = 0
     for line in lines:
         x = line.strip('\n')
         x = x.split(' ')
-        if(len(x) == 2):
+        count = count + 1
+        if(len(x) == 1):
+            data.append(sub_data)
+            sub_data = []
+        elif count == eol_check:
+            data.append(sub_data)
+            sub_data = []
+        elif (len(x) == 2):
             spin = float(x[0])
             energy = float(x[1])
-            # print(spin, energy)
             data_tuple = [spin, energy]
-            data.append(data_tuple)
-    print(data)
+            sub_data.append(data_tuple)
+for big_data in data:
+    print(big_data)
