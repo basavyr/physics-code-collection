cebcef37e96d1155c02e00245f4bd0c7157aa87d
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-07 06:21:51 +0300

    adds numerical data for Ru isotopes into python module. optimized reading of data from files

--- Code-Collection/Energy-Staggering-Wobbling/python/staggering.py
+++ Code-Collection/Energy-Staggering-Wobbling/python/staggering.py
@@ -4,43 +4,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-RU_108 = '108Ru.dat'
-RU_110 = '110Ru.dat'
-RU_112 = '112Ru.dat'
+RU_108_FILE = '108Ru.dat'
+RU_110_FILE = '110Ru.dat'
+RU_112_FILE = '112Ru.dat'
 
 
 def Import_Data(FILE):
     with open(FILE, 'r+') as DATA_STACK:
         lines = DATA_STACK.readlines()
+        lines.pop(0)
+        # print(lines)
         DATA = []
         sub_data = []
         eol_check = len(lines)
         count = 0
-        for line in lines:
-            x = line.strip('\n')
-            x = x.split(' ')
-            count = count + 1
-            # print(x, count)
-            if (len(x) == 2):
-                # print('IN STACK-BUILD')
+        DEBUG = 1
+        if(DEBUG == 1):
+            for line in lines:
+                x = line.strip('\n')
+                x = x.split(' ')
+                count = count + 1
                 # print(x, count)
-                spin = float(x[0])
-                energy = float(x[1])
-                data_tuple = [spin, energy]
-                sub_data.append(data_tuple)
-            if (count == eol_check and len(x) == 2):
-                # print('IN EOL CHECK')
-                # print(sub_data)
-                # print(DATA)
-                DATA.append(sub_data)
-                # print(DATA)
-                sub_data = []
-            elif (len(x) == 1):
-                # print('IN MULTI-BAND')
-                # print(sub_data)
-                DATA.append(sub_data)
-                # print(DATA)
-                sub_data = []
+                if (len(x) == 2):
+                    # print('IN STACK-BUILD')
+                    # print(x, count)
+                    spin = float(x[0])
+                    energy = float(x[1])
+                    data_tuple = [spin, energy]
+                    sub_data.append(data_tuple)
+                if (count == eol_check and len(x) == 2):
+                    # print('IN EOL CHECK')
+                    # print(sub_data)
+                    # print(DATA)
+                    DATA.append(sub_data)
+                    # print(DATA)
+                    sub_data = []
+                elif (len(x) == 1 or x[0] == '-'):
+                    # print('IN MULTI-BAND')
+                    # print(sub_data)
+                    DATA.append(sub_data)
+                    # print(DATA)
+                    sub_data = []
     if(len(DATA) == 0):
         return 'The experimental data stack is empty!'
     else:
@@ -56,5 +60,15 @@ def Stagger_Parameter(I, E1, E2):
 # As defined by Uma et al (2015)
 
 
-for stack in Import_Data(RU_112):
+RU_108_DATA = Import_Data(RU_108_FILE)
+RU_110_DATA = Import_Data(RU_110_FILE)
+RU_112_DATA = Import_Data(RU_112_FILE)
+
+# for stack in RU_108_DATA:
+#     print(stack)
+
+# for stack in RU_110_DATA:
+#     print(stack)
+
+for stack in RU_112_DATA:
     print(stack)
