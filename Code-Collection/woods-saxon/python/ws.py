748b92d436eb84c8440a12e1601cdf5951f4ea31
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-12-27 21:09:33 +0200

    add python ws calculator

--- Code-Collection/woods-saxon/python/ws.py
+++ Code-Collection/woods-saxon/python/ws.py
@@ -9,3 +9,21 @@ def Test_NP_MODULE():
         print('All tests passed')
 
 Test_NP_MODULE()
+
+def Woods_Saxon(V0,R0,s0):
+    """
+    Returns the expression of the Woods-Saxon potential
+    The Fermi-function which aims at describing the nuclear charge distribution within a nucleus
+    """
+    S1=-V0
+    EXP_F=lambda r: 1+np.exp((r-R0)/s0)
+    S2=lambda r: (1+EXP_F(r))
+
+    V_WS=[]
+    for radius in range(0,5):
+        V_WS.append(S2(radius))
+    
+    return V_WS
+
+x=Woods_Saxon(20,1,1)
+print(x)
