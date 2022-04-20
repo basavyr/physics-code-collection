1b7f375efd4f741935a25784fbab976daef0f352
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-04-20 11:52:23 +0300

    proper energy calculation

--- Code-Collection/Wobbling-130Ba/src/fragmented_frequency.py
+++ Code-Collection/Wobbling-130Ba/src/fragmented_frequency.py
@@ -41,14 +41,28 @@ def omega(I, I1, I2, I3):
         print(I1, I2, I3, err)
         return MAXVAL
     else:
-        return t_sqrt
+        omega_res = 2.0*I*t_sqrt
+        return omega_res
 
 
-with open(omega_file, 'w+') as writer:
-    writer.writelines("I1 I2 I3 t1 t2\n")
-    for I1 in np.arange(-1, 6, 1):
-        for I2 in np.arange(-1, 6, 1):
-            for I3 in np.arange(-1, 6, 1):
-                freq = omega(10, I1, I2, I3)
-                if(freq is not MAXVAL):
-                    writer.writelines(f'{I1} {I2} {I3} {freq}\n')
+def energy(n, I, I1, I2, I3):
+    homega = omega(I, I1, I2, I3)
+    if (homega == MAXVAL):
+        return MAXVAL
+    e = 1.0/(2.0*I3)*I*(I+1.0)+(n+0.5)*homega
+    if (e == MAXVAL):
+        return MAXVAL
+
+    return e
+
+
+def write_energy_to_file():
+    with open(omega_file, 'w+') as writer:
+        writer.writelines("I1 I2 I3 omega E\n")
+        for I1 in np.arange(-10, 120, 1):
+            for I2 in np.arange(-10, 120, 1):
+                for I3 in np.arange(-10, 120, 1):
+                    freq = omega(10, I1, I2, I3)
+                    e = energy(0, 10, I1, I2, I3)
+                    if(freq is not MAXVAL):
+                        writer.writelines(f'{I1} {I2} {I3} {freq} {e}\n')
