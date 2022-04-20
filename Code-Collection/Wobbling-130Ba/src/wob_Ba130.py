587fd2533c6f72127c9e0cc027e9ea3761f813fe
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-04-20 08:47:08 +0300

    add excitation energies into the formulas

--- Code-Collection/Wobbling-130Ba/src/wob_Ba130.py
+++ Code-Collection/Wobbling-130Ba/src/wob_Ba130.py
@@ -23,6 +23,15 @@ PHONON_BAND1 = [0 for _ in range(len(SPINS_BAND1))]
 PHONON_BAND2 = [1 for _ in range(len(SPINS_BAND2))]
 
 
+# generate excitation energies from the absolute values
+bandhead = ENERGIES_BAND1[0]
+spinhead = SPINS_BAND1[0]
+EXC_ENERGY1 = [e-bandhead for e in ENERGIES_BAND1[1:]]
+EXC_ENERGY2 = [e-bandhead for e in ENERGIES_BAND2]
+SPINS_BAND1 = SPINS_BAND1[1:]
+PHONON_BAND1 = PHONON_BAND1[1:]
+
+
 # define the general energy formula
 # the Energy depends on two parameters: 3rd MOI and the wobbling frequency
 def Energy(n, spin, I1, I2, I3):
@@ -39,20 +48,25 @@ def Energy(n, spin, I1, I2, I3):
         return MAXVAL
 
     # calculate the energy term coming from the rotational motion around the 3-axis
+    T_rotor_0 = 1/(2.0*I3)*spinhead*(spinhead+1.0)
     T_rotor = 1/(2.0*I3)*spin*(spin+1.0)
 
     # calculate the energy coming from the contribution of axes (1,2) as small amplitude oscillations
+    T_wobbling_0 = 2*spinhead*np.sqrt((1/(2.0*I1)-1/(2.0*I3))
+                                      * (1/(2.0*I2)-1/(2.0*I3)))*(n+0.5)
     T_wobbling = 2*spin*np.sqrt((1/(2.0*I1)-1/(2.0*I3))
                                 * (1/(2.0*I2)-1/(2.0*I3)))*(n+0.5)
 
+    e_bandhead = T_wobbling_0+T_rotor_0
+
     T = T_rotor+T_wobbling
 
-    return T
+    return T-e_bandhead
 
 
 # define the general energy formula using generalized MOI I_i=theta_I(1+cI)
 # the Energy depends on two parameters: 3rd MOI and the wobbling frequency
-def Energy(n, spin, c, theta1, theta2, theta3):
+def Energy_c(n, spin, c, theta1, theta2, theta3):
     I1 = theta1*(1+c*spin)
     I2 = theta2*(1+c*spin)
     I3 = theta3*(1+c*spin)
@@ -81,10 +95,10 @@ def Energy(n, spin, c, theta1, theta2, theta3):
 
 
 # update the model to include 4 parameters: c, theta_{1,2,3}
-def modelFunction(data, p0, p1, p2, p3):
+def modelFunction(data, p1, p2, p3):
     wobbling_phonons, spins = data
 
-    model_energies = Energy(wobbling_phonons, spins, p0, p1, p2, p3)
+    model_energies = Energy(wobbling_phonons, spins, p1, p2, p3)
 
     return model_energies
 
@@ -119,23 +133,14 @@ def fit_full_bands():
     phonons = np.asarray(PHONON_BAND1+PHONON_BAND2)
 
     # keep the energies in the same form
-    energies = ENERGIES_BAND1+ENERGIES_BAND2
+    energies = EXC_ENERGY1+EXC_ENERGY2
 
-    normalize = False
-    # use normalized data
-    if normalize == True:
-        band_head = energies[0]
-        energies = [e-band_head for e in energies]
-        energies = energies[1:]
-        spins = spins[1:]
-        phonons = phonons[1:]
-
-    initial_params = [0.5, 1, 5, 12]
+    initial_params = [1, 5, 12]
     popt, pcov = curve_fit(
         modelFunction, (spins, phonons), energies, initial_params)
 
     return popt
 
 
-FIT = fit_band_1()
+FIT = fit_full_bands()
 print(FIT)
