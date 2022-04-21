ea968e1e280a0aaf2a34883483657bc3f4bfc3a7
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-04-21 10:14:38 +0300

    refactor the model fit

--- Code-Collection/Wobbling-130Ba/src/Fitting_Model_130Ba.py
+++ Code-Collection/Wobbling-130Ba/src/Fitting_Model_130Ba.py
@@ -1,11 +1,13 @@
 import numpy as np
 import math
 from scipy.optimize import curve_fit
-from matplotlib import pyplot as plt
+
+
 # escape value to be used in case of math failure
 MAXVAL = 6969696969
 
 
+# transform the energy from keV to MeV
 def MeV(energy):
     return np.round(energy/1000.0, 3)
 
@@ -19,17 +21,23 @@ ENERGIES2 = [4456.2, 4986.2, 5647.2,
              6442.2, 7319.3, 8265.3, 9283.3, 10436.3]
 ENERGIES_BAND1 = list(map(MeV, ENERGIES1))
 ENERGIES_BAND2 = list(map(MeV, ENERGIES2))
-bandhead = ENERGIES_BAND1[0]
-spinhead = SPINS_BAND1[0]
+BANDHEAD = ENERGIES_BAND1[0]
+SPINHEAD = SPINS_BAND1[0]
 PHONON_BAND1 = [0 for _ in range(len(SPINS_BAND1))]
 PHONON_BAND2 = [1 for _ in range(len(SPINS_BAND2))]
 
 
 # generate excitation energies from the absolute values
-EXC_ENERGY1 = [e-bandhead for e in ENERGIES_BAND1[1:]]
-EXC_ENERGY2 = [e-bandhead for e in ENERGIES_BAND2]
+EXC_ENERGY1 = [e-BANDHEAD for e in ENERGIES_BAND1[1:]]
+EXC_ENERGY2 = [e-BANDHEAD for e in ENERGIES_BAND2]
 SPINS_BAND1 = SPINS_BAND1[1:]
 PHONON_BAND1 = PHONON_BAND1[1:]
+
+
+# create joint data-sets
+spins = np.asarray(SPINS_BAND1+SPINS_BAND2)
+phonons = np.asarray(PHONON_BAND1+PHONON_BAND2)
+experimental_energies = EXC_ENERGY1+EXC_ENERGY2
 
 
 # define a fragmented wobbling frequency for the rotor Hamiltonian
@@ -66,6 +74,7 @@ def omega(I, I1, I2, I3):
         print(I1, I2, I3, err)
         return MAXVAL
     else:
+        # return the wobbling frequency if the term under the square root is positive
         omega_res = 2.0*I*t_sqrt
         return omega_res
 
@@ -84,7 +93,7 @@ def energy(n, I, I1, I2, I3):
 
 # define the excitation energy for the triaxial rotator
 def exc_energy(n, I, I1, I2, I3):
-    e0 = energy(0, spinhead, I1, I2, I3)
+    e0 = energy(0, SPINHEAD, I1, I2, I3)
     # if(e0 == MAXVAL):
     #     return MAXVAL
     e = energy(n, I, I1, I2, I3)
@@ -94,9 +103,8 @@ def exc_energy(n, I, I1, I2, I3):
     return e-e0
 
 
-def model_energy(xdata, I1, I2, I3):
-    phonons, spins = xdata
-
+def model_energy(x_data, I1, I2, I3):
+    phonons, spins = x_data
     local_energy = exc_energy(phonons, spins, I1, I2, I3)
 
     # if(local_energy == MAXVAL):
@@ -117,21 +125,32 @@ def write_energy_to_file():
                         writer.writelines(f'{I1} {I2} {I3} {e}\n')
 
 
-# write_energy_to_file()
-
-spins = np.asarray(SPINS_BAND1+SPINS_BAND2)
-phonons = np.asarray(PHONON_BAND1+PHONON_BAND2)
-energies = EXC_ENERGY1+EXC_ENERGY2
-xdata = (phonons, spins)
+# create a tuple for the x-data which will be used within the model fit
+x_data = (phonons, spins)
 PARAMS_BOUNDS = ([0.5, 0.5, 0.5], [120, 120, 120])
 
 
+# start the model fit using the triaxial rigid rotor energy
 p0 = [10, 1, 3]
-
-popt, pcov = curve_fit(model_energy, xdata, energies,
+popt, pcov = curve_fit(model_energy, x_data, experimental_energies,
                        p0, bounds=PARAMS_BOUNDS)
 
-print(popt[0], popt[1], popt[2])
+
+# print the parameters to console
+def prin_params(params):
+    print(params[0], params[1], params[2])
+
+
+# write
+def write_params(params):
+    with open("/Users/basavyr/Documents/Work/DFT/mathematica-useful-algorithms/Physics/wobbling-ba130/fit_params.dat", 'w+') as writer:
+        writer.write(str(params[0]))
+        writer.write("\n")
+        writer.write(str(params[1]))
+        writer.write("\n")
+        writer.write(str(params[2]))
+        writer.write("\n")
+
 
 theoretical_data_1 = [exc_energy(
     0, SPINS_BAND1[idx], popt[0], popt[1], popt[2]) for idx in range(len(SPINS_BAND1))]
@@ -139,12 +158,15 @@ theoretical_data_2 = [exc_energy(
     1, SPINS_BAND2[idx], popt[0], popt[1], popt[2]) for idx in range(len(SPINS_BAND2))]
 
 
-plt.plot(SPINS_BAND1, EXC_ENERGY1, 'ok', label='band1 exp')
-plt.plot(SPINS_BAND1, theoretical_data_1, '-k', label='band1 th')
-plt.plot(SPINS_BAND2, EXC_ENERGY2, 'or', label='band2 exp')
-plt.plot(SPINS_BAND2, theoretical_data_2, '-r', label='band2 th')
-plt.legend(loc='best')
-plt.xlabel('I')
-plt.ylabel('E')
-plt.savefig('wobbling_results.pdf', dpi=300, bbox_inches='tight')
-plt.close()
+with open("/Users/basavyr/Documents/Work/DFT/mathematica-useful-algorithms/Physics/wobbling-ba130/ba_130_B1_band.dat", 'w+') as writer:
+    for idx in range(len(SPINS_BAND1)):
+        data_line = f'{SPINS_BAND1[idx]} {EXC_ENERGY1[idx]} {theoretical_data_1[idx]}'
+        writer.write(data_line)
+        writer.write("\n")
+
+
+with open("/Users/basavyr/Documents/Work/DFT/mathematica-useful-algorithms/Physics/wobbling-ba130/ba_130_B2_band.dat", 'w+') as writer:
+    for idx in range(len(SPINS_BAND2)):
+        data_line = f'{SPINS_BAND2[idx]} {EXC_ENERGY2[idx]} {theoretical_data_2[idx]}'
+        writer.write(data_line)
+        writer.write("\n")
