4a93ae716eba31574b9754880b7b4cc305cc95c1
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-05-16 16:46:20 +0300

    add proper frame labels

--- Code-Collection/Classical-Energy-Function/src/a_varphi.py
+++ Code-Collection/Classical-Energy-Function/src/a_varphi.py
@@ -24,28 +24,31 @@ plt.rcParams.update({
     "text.usetex": True})
 
 # use custom size
-# soource: https://stackoverflow.com/questions/332289/how-do-i-change-the-size-of-figures-drawn-with-matplotlib
+# source: https://stackoverflow.com/questions/332289/how-do-i-change-the-size-of-figures-drawn-with-matplotlib
 plt.rcParams["figure.figsize"] = (5, 4)
 
-#change the font size
+# change the font size
+# source: https://stackoverflow.com/questions/3899980/how-to-change-the-font-size-on-a-matplotlib-plot
 plt.rcParams.update({'font.size': 12})
 
 # create the first plot
-plt.xlabel(r'$\varphi$')
-plt.ylabel(r'$\mathcal{A}_\varphi$')
+plt.xlabel(r'$\varphi$ [rad]')
+plt.ylabel(r'$\mathcal{A}_\varphi\ [\hbar^{-2}$MeV$]$')
 plt.plot(x, y1, '-r', label=r'$A_1<A_2<A_3$')
 plt.plot(x, y2, '-b', label=r'$A_1<A_3<A_2$')
 plt.plot(x, y3, '-k', label=r'$A_3<A_2<A_1$')
 plt.legend(loc='best')
+plt.tight_layout()
 plt.savefig(f'{plot_location}/A_varphi_1.pdf', dpi=300)
 plt.close()
 
 # create the second plot
-plt.xlabel(r'$\varphi$')
-plt.ylabel(r'$\mathcal{A}_\varphi$')
+plt.xlabel(r'$\varphi$ [rad]')
+plt.ylabel(r'$\mathcal{A}_\varphi\ [\hbar^{-2}$MeV$]$')
 plt.plot(x, y4, '-r', label=r'$A_2<A_3<A_1$')
 plt.plot(x, y5, '-b', label=r'$A_2<A_1<A_3$')
 plt.plot(x, y6, '-k', label=r'$A_3<A_1<A_3$')
 plt.legend(loc='best')
+plt.tight_layout()
 plt.savefig(f'{plot_location}/A_varphi_2.pdf', dpi=300)
 plt.close()
