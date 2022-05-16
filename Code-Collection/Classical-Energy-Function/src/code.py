adfb5cf68d72c765003ebfc7be4939a55cf7a777
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-05-16 12:17:02 +0300

    add proper canonical factors

--- Code-Collection/Classical-Energy-Function/src/code.py
+++ Code-Collection/Classical-Energy-Function/src/code.py
@@ -16,21 +16,48 @@ class Canonical:
     """
 
     @staticmethod
+    def rad(angle):
+        return float(angle*np.pi/180.0)
+
+    @staticmethod
     def A_varphi(inertia_factors, varphi):
-        return 1
+        A1 = inertia_factors[0]
+        A2 = inertia_factors[1]
+        A3 = inertia_factors[2]
+
+        A = A1*np.power(np.cos(Canonical.rad(varphi)), 2)+A2 * \
+            np.power(np.sin(Canonical.rad(varphi)), 2)-A3
+
+        return A
 
     @staticmethod
     def A_psi(inertia_factors, psi):
-        return 1
+        A1 = inertia_factors[0]
+        A2 = inertia_factors[1]
+        A3 = inertia_factors[2]
+
+        A = A1*np.power(np.cos(Canonical.rad(psi)), 2)+A2 * \
+            np.power(np.sin(Canonical.rad([psi])), 2)-A3
+
+        return A
 
     # define the mixed canonical factor which contains both the varphi and phi coordinates
     @staticmethod
     def A_mixed(inertia_factors, varphi, psi):
-        return 1
+        A1 = inertia_factors[0]
+        A2 = inertia_factors[1]
+
+        A = A1*np.cos(Canonical.rad(varphi))*np.cos(Canonical.rad(psi)) + \
+            A2*np.sin(Canonical.rad(varphi))*np.sin(Canonical.rad(psi))
 
     @staticmethod
     def A_gamma(gamma, j, f, psi):
-        return 1
+        rad3 = np.sqrt(3.0)
+
+        A = np.cos(Canonical.rad(gamma))-(f*(2.0*j-f))/(2.0*j*j)*rad3*(rad3*np.cos(
+            Canonical.rad(gamma))+np.sin(Canonical.rad(gamma))*np.cos(2*Canonical.rad(psi)))
+
+        return A
 
 
-print(Canonical.A_varphi(1, 1))
+print(Canonical.A_varphi([11, 1, 1], 1))
