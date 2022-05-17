a52d2f7499138d39e1c81347dde801cb5149654a
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-05-17 08:38:10 +0300

    proper layout spacings between the grid subplot

--- Code-Collection/Classical-Energy-Function/src/a_mixed.py
+++ Code-Collection/Classical-Energy-Function/src/a_mixed.py
@@ -30,7 +30,7 @@ plt.rcParams.update({
 
 # use custom size
 # source: https://stackoverflow.com/questions/332289/how-do-i-change-the-size-of-figures-drawn-with-matplotlib
-plt.rcParams["figure.figsize"] = (13, 7)
+plt.rcParams["figure.figsize"] = (9, 9)
 # plt.rcParams["figure.figsize"] = (8,5)
 # reset the plot size
 # plt.rcParams['figure.figsize'] = plt.rcParamsDefault['figure.figsize']
@@ -38,9 +38,9 @@ plt.rcParams["figure.figsize"] = (13, 7)
 
 # change the font size
 # source: https://stackoverflow.com/questions/3899980/how-to-change-the-font-size-on-a-matplotlib-plot
-plt.rcParams.update({'font.size': 17})
+plt.rcParams.update({'font.size': 16})
 
-plt.rc('axes', titlesize=15)  # fontsize of the figure title
+plt.rc('axes', titlesize=14)  # fontsize of the figure title
 
 # plt.rcParams["contour.linewidth"] = 1.1
 plt.rcParams["image.cmap"] = 'RdBu_r'
@@ -48,10 +48,13 @@ plt.rcParams["image.cmap"] = 'RdBu_r'
 
 
 # fig, ax = plt.subplots(2, 3, sharex=True, sharey=True)
-fig = plt.figure()
+# fig = plt.figure()
 
-gs = fig.add_gridspec(2, 3, hspace=0.11,wspace=0.1)
-ax = gs.subplots(sharex=True, sharey=True)
+# gs = fig.add_gridspec(3, 2, hspace=0.13, wspace=0.1)
+# ax = gs.subplots(sharex=True, sharey=True)
+
+fig, ax = plt.subplots(nrows=3, ncols=2,sharex=True,sharey=True)
+
 
 plt.xticks(np.arange(0, 181, 60))
 plt.yticks(np.arange(0, 181, 60))
@@ -60,18 +63,18 @@ im1 = ax[0, 0].contourf(X, Y, Z_123, levels=21, antialiased=True)
 ax[0, 0].set_title(r'$A_1<A_2<A_3$')
 im2 = ax[0, 1].contourf(X, Y, Z_132,  levels=21, antialiased=True)
 ax[0, 1].set_title(r'$A_1<A_3<A_2$')
-im3 = ax[0, 2].contourf(X, Y, Z_213,  levels=21, antialiased=True)
-ax[0, 2].set_title(r'$A_2<A_1<A_3$')
-im4 = ax[1, 0].contourf(X, Y, Z_231,  levels=21, antialiased=True)
-ax[1, 0].set_title(r'$A_2<A_3<A_1$')
-ax[1, 0].set(xlabel=r'$\varphi$ $[^\circ]$', ylabel=r'$\psi$ $[^\circ]$')
-im5 = ax[1, 1].contourf(X, Y, Z_312,  levels=21, antialiased=True)
-ax[1, 1].set_title(r'$A_3<A_1<A_2$')
-im6 = ax[1, 2].contourf(X, Y, Z_321,  levels=21, antialiased=True)
-ax[1, 2].set_title(r'$A_3<A_2<A_1$')
+im3 = ax[1, 0].contourf(X, Y, Z_213,  levels=21, antialiased=True)
+ax[1, 0].set_title(r'$A_2<A_1<A_3$')
+im4 = ax[1, 1].contourf(X, Y, Z_231,  levels=21, antialiased=True)
+ax[1, 1].set_title(r'$A_2<A_3<A_1$')
+im5 = ax[2, 0].contourf(X, Y, Z_312,  levels=21, antialiased=True)
+ax[2, 0].set_title(r'$A_3<A_1<A_2$')
+ax[2, 0].set(xlabel=r'$\varphi$ $[^\circ]$', ylabel=r'$\psi$ $[^\circ]$')
+im6 = ax[2, 1].contourf(X, Y, Z_321,  levels=21, antialiased=True)
+ax[2, 1].set_title(r'$A_3<A_2<A_1$')
 
 imgs = [im1, im2, im3, im4, im5, im6]
-axis_list = [ax[0, 0], ax[0, 1], ax[0, 2], ax[1, 0], ax[1, 1], ax[1, 2]]
+axis_list = [ax[0, 0], ax[0, 1], ax[1, 0], ax[1, 1], ax[2, 0], ax[2, 1]]
 
 for bars in zip(imgs, axis_list):
     fig.colorbar(bars[0], ax=bars[1])
@@ -80,6 +83,7 @@ for bars in zip(imgs, axis_list):
 #     axx.set(xlabel='x-label', ylabel='y-label')
 
 plt.tight_layout()
+plt.subplots_adjust(wspace=0.1, hspace=0.15)
 plt.savefig(f'{figures_path_mini}/A_mixed.pdf', dpi=300, pad_inches=0)
 plt.savefig(f'{plot_location}/A_mixed.pdf', dpi=300, pad_inches=0)
 plt.close()
