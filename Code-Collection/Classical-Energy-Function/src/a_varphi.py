2a0a800830b7aaa92ff6da5c034ca1b48f35d9d9
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-05-17 05:34:35 +0300

    improve plot styling and add pipfile

--- Code-Collection/Classical-Energy-Function/src/a_varphi.py
+++ Code-Collection/Classical-Energy-Function/src/a_varphi.py
@@ -5,6 +5,8 @@ import factors as A
 
 plot_location = './results'
 
+# path to the figure folder on MacBook Air
+figure_path_air = "/Users/basavyr/Documents/Work/PhD/phdthesis/Chapters/Figures"
 
 # generate angles
 x = np.arange(0, 180, 5)
@@ -23,34 +25,39 @@ y6 = A.Canonical.A_varphi([3, 7, 1], x)
 plt.rcParams.update({
     "text.usetex": True})
 
+
 # use custom size
 # source: https://stackoverflow.com/questions/332289/how-do-i-change-the-size-of-figures-drawn-with-matplotlib
-plt.rcParams["figure.figsize"] = (8,6)
+# plt.rcParams["figure.figsize"] = (5, 4)
+plt.rcParams["figure.figsize"] = (7, 6)
+# reset the plot size
+# plt.rcParams['figure.figsize'] = plt.rcParamsDefault['figure.figsize']
+
 
 # change the font size
 # source: https://stackoverflow.com/questions/3899980/how-to-change-the-font-size-on-a-matplotlib-plot
-plt.rcParams.update({'font.size': 24})
+plt.rcParams.update({'font.size': 26})
 
 # create the first plot
-plt.xlabel(r'$\varphi$ [rad]')
+plt.xlabel(r'$\varphi$ $[^\circ]$')
 plt.ylabel(r'$\mathcal{A}_\varphi\ [\hbar^{-2}$MeV$]$')
 plt.plot(x, y1, '-r', label=r'$A_1<A_2<A_3$')
 plt.plot(x, y2, '-b', label=r'$A_1<A_3<A_2$')
 plt.plot(x, y3, '-k', label=r'$A_3<A_2<A_1$')
-plt.rc('legend', fontsize=15)    # legend fontsize
-plt.legend(loc='best')
+plt.rc('legend', fontsize=22)    # legend fontsize
+plt.legend(loc='best', framealpha=0.0)
 plt.tight_layout()
-plt.savefig(f'{plot_location}/A_varphi_1.pdf', dpi=300)
+plt.savefig(f'{figure_path_air}/A_varphi_1.pdf', dpi=300)
 plt.close()
 
 # create the second plot
-plt.xlabel(r'$\varphi$ [rad]')
+plt.xlabel(r'$\varphi$ $[^\circ]$')
 plt.ylabel(r'$\mathcal{A}_\varphi\ [\hbar^{-2}$MeV$]$')
 plt.plot(x, y4, '-r', label=r'$A_2<A_3<A_1$')
 plt.plot(x, y5, '-b', label=r'$A_2<A_1<A_3$')
-plt.plot(x, y6, '-k', label=r'$A_3<A_1<A_3$')
-plt.rc('legend', fontsize=15)    # legend fontsize
-plt.legend(loc='best')
+plt.plot(x, y6, '-k', label=r'$A_3<A_1<A_2$')
+plt.rc('legend', fontsize=22)    # legend fontsize
+plt.legend(loc='best', framealpha=0.0)
 plt.tight_layout()
-plt.savefig(f'{plot_location}/A_varphi_2.pdf', dpi=300)
+plt.savefig(f'{figure_path_air}/A_varphi_2.pdf', dpi=300)
 plt.close()
