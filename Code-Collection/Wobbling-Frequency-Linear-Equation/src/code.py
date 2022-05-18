d82b3f9a59d956d4de865acc3c47555540dcb041
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-05-18 07:18:39 +0300

    add skeleton for the equation solver

---
+++ Code-Collection/Wobbling-Frequency-Linear-Equation/src/code.py
@@ -0,0 +1,18 @@
+from sympy.solvers import solve
+from sympy import Symbol
+import numpy as np
+
+
+G1 = Symbol('G1')
+G2 = Symbol('G2')
+P_31 = Symbol('P_31')
+P_21 = Symbol('P_21')
+Q_31 = Symbol('Q_31')
+Q_21 = Symbol('Q_21')
+I = Symbol('I')
+j = Symbol('j')
+barA_2 = Symbol('Å_2')
+barA_3 = Symbol('Å_3')
+
+# main variable
+S = Symbol('S')
