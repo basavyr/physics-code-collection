05dbd1dbade4ff7f7f551543f7648197e7165436
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-05-27 10:27:55 +0300

    refactor the Delta and Sigma

--- Code-Collection/Canonicity-Factors/src/common_factors.py
+++ Code-Collection/Canonicity-Factors/src/common_factors.py
@@ -9,7 +9,7 @@ class Factors:
 
     rad3 = np.sqrt(3.0)
 
-    # define the Sigma and Delta terms from Eq. 6.44
+    # define the Delta terms from Eq. 6.44
     @staticmethod
     def Delta_I(Spin, OddSpin, A1, A):
         # re-define spins for cohesive formulas
@@ -18,21 +18,21 @@ class Factors:
 
         Delta_A = (A-A1)
         Const_j = 2.0*j*A1
-        Delta = (2.0*I-1.0)*Delta_A+Const_j
 
+        Delta = (2.0*I-1.0)*Delta_A+Const_j
         return Delta
 
-    # define the Sigma and Delta terms from Eq. 6.44
+    # define the Sigma terms from Eq. 6.44
     @staticmethod
-    def Delta_j(Spin, OddSpin, A1, A):
+    def Sigma_j(Spin, OddSpin, A1, A):
         # re-define spins for cohesive formulas
         I = Spin
         j = OddSpin
 
         Delta_A = (A-A1)
-        Const_I = 2.0*A1
-        Delta = (2.0*j-1.0)*Delta_A+Const_I
+        Const_I = 2.0*I*A1
 
+        Delta = (2.0*j-1.0)*Delta_A+Const_I
         return Delta
 
     # define the G-terms from Eq. 6.26
--- Code-Collection/Canonicity-Factors/src/k1-k2.py
+++ Code-Collection/Canonicity-Factors/src/k1-k2.py
@@ -4,3 +4,13 @@ import matplotlib.pyplot as plt
 
 # local import
 from common_factors import Factors as F
+
+
+# define k1 according to Eq. 6.46
+def k1(Spin, OddSpin, A1, A2, A3):
+    return 1
+
+
+# define k2 according to Eq. 6.46
+def k2(Spin, OddSpin, A1, A2, A3):
+    return 2
