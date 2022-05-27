b3e4da88d974db30a01b58ead40b47a324e74ab7
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-05-27 14:41:35 +0300

    Update om1-om2.py

--- Code-Collection/Canonicity-Factors/src/om1-om2.py
+++ Code-Collection/Canonicity-Factors/src/om1-om2.py
@@ -29,8 +29,8 @@ def omega2(Spin, OddSpin, A1, A2, A3, V, gamma):
     G_1 = F.G_1(j, gm)
     G_2 = F.G_2(j, gm)
 
-    T0_1 = Sigma_21+V*F.G_2(j, gm)
-    T0_2 = Sigma_31+V*F.G_1(j, gm)
+    T0_1 = Sigma_21+V*F.G_1(j, gm)
+    T0_2 = Sigma_31+V*F.G_2(j, gm)
 
     omega = np.sqrt(T0_1)*np.sqrt(T0_2)
 
@@ -38,10 +38,19 @@ def omega2(Spin, OddSpin, A1, A2, A3, V, gamma):
 
 
 # define the constants
-A1 = 0.2
+A1 = 0.05
 I0 = 35.0/2.0
 j0 = 13.0/2.0
 V0 = 3.0
 gm0 = 25.0*np.pi/180.0
 
-spins = np.arange(0.5, 45.4, 2)
+spins = np.arange(1.5, 42.5, 2)
+
+omega1_data_1 = omega1(spins, j0, A1, 2*A1, 5*A1)
+omega1_data_2 = omega1(spins, j0, A1, 5*A1, 2*A1)
+print(omega1_data_1)
+print(omega1_data_2)
+omega2_data_1 = omega2(spins, j0, A1, 2*A1, 5*A1, V0, gm0)
+omega2_data_2 = omega2(spins, j0, A1, 5*A1, 2*A1, V0, gm0)
+print(omega2_data_1)
+print(omega2_data_2)
