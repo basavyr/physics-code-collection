4fb6c535b480bc97eb23af0b0bcb09c2fa4d9f02
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-05-27 13:47:27 +0300

    checks k2 with the math results

--- Code-Collection/Canonicity-Factors/src/k1-k2.py
+++ Code-Collection/Canonicity-Factors/src/k1-k2.py
@@ -29,8 +29,8 @@ def k2(Spin, OddSpin, A1, A2, A3, V, gamma):
     Sigma_21 = F.Sigma_j(I, j, A1, A2)
     Sigma_31 = F.Sigma_j(I, j, A1, A3)
 
-    T0_1 = Sigma_21+V*F.G_2(j, gm)
-    T0_2 = Sigma_31+V*F.G_1(j, gm)
+    T0_1 = Sigma_21+V*F.G_1(j, gm)
+    T0_2 = Sigma_31+V*F.G_2(j, gm)
 
     Term_j = (T0_1/T0_2)*np.power(j, 2)
     k_factor = np.power(Term_j, 0.25)
@@ -45,10 +45,15 @@ j0 = 13.0/2.0
 V0 = 3.0
 gm0 = 25.0*np.pi/180.0
 
-spins = np.arange(0.5, 45.4, 1)
+spins = np.arange(0.5, 45.4, 2)
 
 
 k1_data_1 = k1(spins, j0, A1, 2.0*A1, 5.0*A1)
 k1_data_2 = k1(spins, j0, A1, 5.0*A1, 2.0*A1)
 print(k1_data_1)
 print(k1_data_2)
+
+k2_data_1 = k2(spins, j0, A1, 2.0*A1, 5.0*A1, V0, gm0)
+k2_data_2 = k2(spins, j0, A1, 5.0*A1, 2.0*A1, V0, gm0)
+print(k2_data_1)
+print(k2_data_2)
