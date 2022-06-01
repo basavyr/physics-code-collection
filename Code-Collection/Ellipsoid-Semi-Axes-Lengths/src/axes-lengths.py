df333ac53dcf412e04ce38c4295fcdd5442bcdb2
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-06-01 14:23:28 +0300

    store the maximum length

--- Code-Collection/Ellipsoid-Semi-Axes-Lengths/src/axes-lengths.py
+++ Code-Collection/Ellipsoid-Semi-Axes-Lengths/src/axes-lengths.py
@@ -2,8 +2,11 @@
 # the required parameters are $\gamma$, $\beta_2$
 # each semi-axis has a specific index k
 
+from operator import indexOf
 import numpy as np
 from matplotlib import pyplot as plt
+
+r0 = 1.2
 
 
 def R_k(k, R_0, beta, gamma):
@@ -23,13 +26,26 @@ def R_k(k, R_0, beta, gamma):
     return RK
 
 
+def Show_Axes(beta, gamma):
+    R1 = R_k(1, r0, beta, gamma)
+    R2 = R_k(2, r0, beta, gamma)
+    R3 = R_k(3, r0, beta, gamma)
+
+    axes = {"1": R1,
+            "2": R2,
+            "3": R3,
+            }
+    max_axes = max([ax for _, ax in axes.items()])
+    index_of_max_axes = indexOf(
+        [ax for _, ax in axes.items()], max_axes)+1
+    print(max_axes)
+    print(index_of_max_axes)
+
+
 def main():
-    r0 = 1.2
     beta = 0.35
     gamma = 20
-    print(R_k(1, r0, beta, gamma))
-    print(R_k(2, r0, beta, gamma))
-    print(R_k(3, r0, beta, gamma))
+    Show_Axes(beta, gamma)
 
 
 if __name__ == '__main__':
