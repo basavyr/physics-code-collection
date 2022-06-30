ddfea90138eedabaa8e0988e1adc22af46d38f01
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-06-30 08:35:04 +0300

    add proper good "I" calculation

--- Code-Collection/Wobbling-Exccitation-Correlation/python/src/ba-130-fit-corrrelation.py
+++ Code-Collection/Wobbling-Exccitation-Correlation/python/src/ba-130-fit-corrrelation.py
@@ -17,17 +17,26 @@ def Ik(inertia_factor):
 
 def Physical_Conditions(A1, A2, A3):
     # skip the non-physical solutions
+    if(A1 < A3 or A2 < A3):
+        return 0
     if(A1 == A3):
         return 0
     if(A2 == A3):
         return 0
     if(A1 == A2):
         return 0
+    if(A1 == 0 or A2 == 0 or A3 == 0):
+        return 0
     if(A1 < A3 and A3 < A2):
         return 0
     if(A3 < A1 and A2 < A3):
         return 0
-    
+
+    t1 = np.power((A2+A1-2.0*A3), 2)
+    t2 = np.power((A2-A1), 2)
+    if(t1 < t2):
+        return 0
+
     # return true if the proper conditions for the inertia factors are net
     return 1
 
@@ -66,14 +75,28 @@ def Wobbling_Frequency(spin, A1, A2, A3):
     return omega
 
 
-ak_values = [Ak(x) for x in np.arange(0.5, 125, 5)]
+ak_values = [Ak(x) for x in np.arange(0.5, 125, 1)]
 
 
 def do_procedure():
+    good_I1 = []
+    good_I2 = []
+    good_I3 = []
+
     for A1 in ak_values:
         for A2 in ak_values:
             for A3 in ak_values:
-                Physical_Conditions(A1, A2, A3)
+                if(Physical_Conditions(A1, A2, A3)):
+                    good_I1.append(Ik(A1))
+                    good_I2.append(Ik(A2))
+                    good_I3.append(Ik(A3))
+
+    plt.plot(good_I1, '-b', label=r'good $\mathcal{J}_1$')
+    plt.plot(good_I2, '-r', label=r'good $\mathcal{J}_2$')
+    plt.plot(good_I3, '-k', label=r'good $\mathcal{J}_3$')
+    plt.legend(loc='best')
+    plt.savefig('physical_mois.pdf', dpi=300)
+    plt.close()
 
 
 if __name__ == '__main__':
