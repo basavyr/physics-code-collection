0d646ad97a8b7e7465dee0329786ac15d6d5f396
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-07-03 07:40:46 +0300

    update

--- Code-Collection/Wobbling-Exccitation-Correlation/python/src/plots.py
+++ Code-Collection/Wobbling-Exccitation-Correlation/python/src/plots.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
 import fitting
+import data130
+import correlations
 
 
 def chi_plotter(exp_data, th_data):
@@ -8,8 +10,8 @@ def chi_plotter(exp_data, th_data):
     spins, experimental_energy = exp_data
     # no need to unpack the spins twice
     _, theoretical_energy = th_data
-    plt.plot(spins, exp_data, 'ok', label='Exp.')
-    plt.plot(spins, th_data, '--*b', label='Th.')
+    plt.plot(spins, experimental_energy, 'ok', label='Exp.')
+    plt.plot(spins, theoretical_energy, '--*b', label='Th.')
     plt.legend(loc='best')
     plt.savefig('chi_plot.pdf', dpi=300)
     plt.close()
@@ -18,6 +20,15 @@ def chi_plotter(exp_data, th_data):
 def main(guess):
     print('Fitting the experimental wobbling energies')
     parameters = fitting.fitting_procedure(guess)
+    exp_data = (data130.Spins.band2, data130.Exp_Wobbling_Energy.band1)
+
+    th_data = (data130.Spins.band2,
+               correlations.Wobbling_Energy_Theoretical(parameters))
+
+    print(exp_data)
+    print('**************')
+    print(th_data)
+    chi_plotter(exp_data, th_data)
 
 
 if __name__ == '__main__':
