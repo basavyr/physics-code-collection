397564838f90b6f58a74387ed435e1bcc6b6203b
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-02-11 17:05:17 +0200

    improve band calculation

--- Code-Collection/New-Boson-Excitation-Energies/src/main.py
+++ Code-Collection/New-Boson-Excitation-Energies/src/main.py
@@ -3,13 +3,16 @@ import numpy as np
 
 
 def main():
-    energy = energies.EnergyFunction(91.0, 9.0, 51.0, 5.5, -119, 5.5)
+    band_head = 5.5
+    theta = -119
+    j = 5.5
+    energy = energies.EnergyFunction(91.0, 9.0, 51.0, j, theta, band_head)
     spins_1 = np.arange(5.5, 27.5, 2)
     spins_2 = np.arange(8.5, 18.5, 2)
     spins_3 = np.arange(9.5, 17.5, 2)
-    band_1 = [energy.excitation_energy(spin, 0) for spin in spins_1]
-    band_2 = [energy.excitation_energy(spin, 0) for spin in spins_2]
-    band_3 = [energy.excitation_energy(spin, 1) for spin in spins_3]
+    band_1 = energy.excitation_energy(spins_1, 0)
+    band_2 = energy.excitation_energy(spins_2, 0)
+    band_3 = energy.excitation_energy(spins_3, 1)
     print("B1")
     print(spins_1)
     print(band_1)
