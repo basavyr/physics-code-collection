cf6391f39848a384397ebe42a759864d9260ceb2
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-02-18 10:14:59 +0200

    fixes the period points for the sn plot

--- Code-Collection/Elliptic-Potential/src/main.py
+++ Code-Collection/Elliptic-Potential/src/main.py
@@ -45,17 +45,18 @@ def make_plot_batch(moi_values: list[float], theta_deg_values: list[float], spin
             elliptic.v_q_func_k_squared, theta_deg, spin_values, plot_name_k_squared)
 
 
-def generate_points(y_0: float, y_1: float, x_values: list[float]) -> list[tuple[list[float], list[float]]]:
+def generate_points(y_0: float, y_1: float, x_values: list[float]) -> list[tuple]:
     """
-    - returns a list of points [(p0_0,p1_0),...] based on a list of x values
-    - p0_0=[x_0,y_0]
-    - p1_0=[x_0,y_1]
+    - returns a list of points [(px_0,py_0),...] based on a list of x values
+    - px_0=[x_0,x_0]
+    - py_0=[y_0,y_1]
     """
     points = []
+    y_points = [y_0, y_1]
     for x in x_values:
-        p0 = [x, y_0]
-        p1 = [x, y_1]
-        points.append((p0, p1))
+        px = [x, x]
+        py = y_points
+        points.append((px, py))
     return points
 
 
@@ -64,35 +65,26 @@ def elliptic_func_comparison(k):
     q_values = np.linspace(0, 7, 100)
     s_values_k = [jacobi.sn_k(q, k) for q in q_values]
     s_values_k_squared = [jacobi.sn_k_squared(q, k) for q in q_values]
-    period = jacobi.period(k)
-    period_line_x_1 = [period for _ in q_values]
-    period_line_x_2 = [2*period for _ in q_values]
-    period_line_x_3 = [3*period for _ in q_values]
-    period_line_x_4 = [4*period for _ in q_values]
-    period_line_y = np.linspace(
-        min(s_values_k), max(s_values_k), len(s_values_k))
-    x_values = [1, 2, 3]
-    N = 2
+
+    periods = [idx*jacobi.period(k) for idx in range(1, 5)]
     y0, y1 = -1, 1
-    points = generate_points(y0, y1, x_values)
+    # generate the list of points for each of the periods 0, K, 2K, 3K, 4K
+    period_points = generate_points(y0, y1, periods)
+
     # add vertical lines to a plot using the axvline function from within matplotlib
     # source: https://stackoverflow.com/questions/52757586/python-plotting-lines-parallel-to-y-axis-from-array
     # for l in x_values:
     #     plotter.plt.axvline(l)
 
-    for p in points:
-        p0, p1 = p
-        print(p0)
-        print(p1)
-    # plots a vertical line at x->1, between -1 and 1
-    plotter.plt.plot([1, 1], [-1, 1], '-k', label='fakey')
-    plotter.plt.plot([3, 3], [-1, 1], '-k', label='fakey')
-    plotter.plt.plot([2, 2], [-1, 1], '-k', label='fakey')
-    # plotter.plt.plot([period_line_x_2], [period_line_y])
-    # plotter.plt.plot(period_line_x_3, period_line_y)
-    # plotter.plt.plot(period_line_x_4, period_line_y)
-    # plotter.plt.plot(q_values, s_values_k, '-r', label=r'$k=m$')
-    # plotter.plt.plot(q_values, s_values_k_squared, '--r',  label=r'$k^2=m$')
+    idx = 1
+    for point in period_points:
+        # unwraps the tuple ([x_0,x_0], [y_0,y_1])
+        p_x, p_y = point
+        # plots a vertical line at x -> x_0, between y0 and y1
+        plotter.plt.plot(p_x, p_y, '--k', label=f'{idx}K')
+        idx = idx+1
+    plotter.plt.plot(q_values, s_values_k, '-r', label=r'$k=m$')
+    plotter.plt.plot(q_values, s_values_k_squared, '--r',  label=r'$k^2=m$')
     plotter.plt.legend(loc='best')
     plotter.plt.savefig(f'../data/elliptic_sn_comparison.pdf',
                         dpi=450, bbox_inches='tight')
