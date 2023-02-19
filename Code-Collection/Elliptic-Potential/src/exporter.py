9fbf5882afa8438bb5705169c172196edbd49d3c
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-02-19 07:59:35 +0200

    archive the elliptic module

--- Code-Collection/Elliptic-Potential/src/exporter.py
+++ Code-Collection/Elliptic-Potential/src/exporter.py
@@ -1,13 +1,13 @@
 import csv
 
 
-def save_to_csv(x_data: list[float], y_data: list[float], file_name: str) -> None:
+def save_to_csv(x_data: list[float], y_data: list[float], file_name: str, header: str) -> None:
     """
     - save the input data to a csv file
     """
     data_pre_path = '../data'
     with open(f'{data_pre_path}/{file_name}.csv', 'w+', encoding='UTF8') as handler:
         writer = csv.writer(handler)
-        writer.writerow([f'idx', 'q', 'V(q)'])
+        writer.writerow([header])
         for idx in range(len(x_data)):
             writer.writerow([idx+1, x_data[idx], y_data[idx]])
