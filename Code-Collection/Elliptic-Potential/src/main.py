e29d8112908028fb52f6ad334b6878098b25dfa5
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-02-19 08:00:35 +0200

    remove create_elliptic_plots

--- Code-Collection/Elliptic-Potential/src/main.py
+++ Code-Collection/Elliptic-Potential/src/main.py
@@ -1,48 +1,7 @@
 import numpy as np
-import elliptic_potential
 import plotter
 import exporter as csv
 import JacobiFunctions as jacobi_func
-
-
-def create_elliptic_plots(elliptic_potential_func: elliptic_potential.EllipticFunctions, theta_deg: float, spin_values: list[float], plot_name: str) -> tuple[list[float], list[float]]:
-    """
-    - create a plot with the elliptic potential V(q)
-    - the plot contains a line for each spin value that is given as argument
-    """
-    x_label = "q [rad]"
-    y_label = r'V(q) [$\hbar^2$]'
-    plot_label = "V(q)"
-    q_x_limit_left = -8.0
-    q_x_limit_right = 8.0
-
-    x_data = np.linspace(q_x_limit_left, q_x_limit_right, 100)
-    y_data = [elliptic_potential_func(
-        spin_values, theta_deg, q) for q in x_data]
-
-    p = plotter.Plotter(x_data, y_data, spin_values)
-    p.make_plot(plot_name, x_label, y_label, plot_label)
-
-    return x_data, y_data
-
-
-def make_plot_batch(moi_values: list[float], theta_deg_values: list[float], spin_values: list[float], odd_spin: float) -> None:
-    """
-    - create a set of plots for a list of theta values (given in degrees)
-    """
-    moi_1, moi_2, moi_3 = moi_values
-    elliptic = elliptic_potential.EllipticFunctions(
-        moi_1, moi_2, moi_3, odd_spin)
-    for theta_deg in theta_deg_values:
-        plot_name_k = f'potential-k-plot-theta-{int(abs(theta_deg))}'
-        plot_name_k_squared = f'potential-k2-plot-theta-{int(abs(theta_deg))}'
-        csv_file_name = f'numerical-data-theta-{int(abs(theta_deg))}'
-        # create the plot for modulus k
-        q_values, v_values_k = create_elliptic_plots(
-            elliptic.v_q_func_k, theta_deg, spin_values, plot_name_k)
-        # create the plot for modulus k^2
-        q_values, v_values_k_squared = create_elliptic_plots(
-            elliptic.v_q_func_k_squared, theta_deg, spin_values, plot_name_k_squared)
 
 
 def generate_points(y_0: float, y_1: float, x_values: list[float]) -> list[tuple]:
@@ -129,8 +88,6 @@ def elliptic_potential(spin_values, mois, oddspin, theta_deg, plot_name):
     for potential in potentials:
         plotter.plt.plot(
             q_values, potential, next(plot_styles), label=f'I={int(2*next(spin_iter))}/2')
-    # plotter.plt.plot(
-    #     q_values, potentials[0], next(plot_styles), label=f'I={int(2*next(spin_iter))}/2')
 
     plotter.plt.legend(loc='best')
     plotter.plt.savefig(f'../data/{plot_name}.pdf',
