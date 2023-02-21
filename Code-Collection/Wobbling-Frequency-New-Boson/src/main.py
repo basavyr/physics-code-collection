3b8a312a365613279f2f030ba699ff65062057da
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-02-21 07:16:00 +0200

    add all fitting params in the main function

--- Code-Collection/Wobbling-Frequency-New-Boson/src/main.py
+++ Code-Collection/Wobbling-Frequency-New-Boson/src/main.py
@@ -15,16 +15,26 @@ def fit_1() -> list[tuple]:
     return numerical_values
 
 
-def fit_2():
+def fit_2() -> list[tuple]:
     params = fit.FittingParameters()
     wobb = wobbling.Wobbling(
         params.MOIS_2, params.ODD_SPIN, params.THETA_DEG_2)
+    spin_values = np.arange(7.5, 41.5, 2)
+    numerical_values = [(spin, wobb.omega(spin), wobb.omega_chiral(spin))
+                        for spin in spin_values]
+
+    return numerical_values
 
 
-def fit_3():
+def fit_3() -> list[tuple]:
     params = fit.FittingParameters()
     wobb = wobbling.Wobbling(
         params.MOIS_3, params.ODD_SPIN, params.THETA_DEG_3)
+    spin_values = np.arange(7.5, 41.5, 2)
+    numerical_values = [(spin, wobb.omega(spin), wobb.omega_chiral(spin))
+                        for spin in spin_values]
+
+    return numerical_values
 
 
 def main():
@@ -33,6 +43,8 @@ def main():
         'I',
         f'omega_{idx}', f'omega_{idx}_pi')
     exporter.export_to_csv(fit_1(), 'data_fit_1', header('1'))
+    exporter.export_to_csv(fit_2(), 'data_fit_2', header('2'))
+    exporter.export_to_csv(fit_3(), 'data_fit_3', header('3'))
 
 
 if __name__ == "__main__":
