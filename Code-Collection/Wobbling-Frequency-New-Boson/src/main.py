3d1ea8db08416b75bf5e3d49861052b7acc5d6ab
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-02-21 06:14:10 +0200

    add skeleton for wobbling and fit params

---
+++ Code-Collection/Wobbling-Frequency-New-Boson/src/main.py
@@ -0,0 +1,10 @@
+import wobbling
+import fit
+
+
+def main():
+    """Main function"""
+
+
+if __name__ == "__main__":
+    main()
---
+++ Code-Collection/Wobbling-Frequency-New-Boson/src/wobbling.py
@@ -0,0 +1,21 @@
+import numpy as np
+from dataclasses import dataclass
+
+
+@dataclass
+class Wobbling:
+    """
+    - A class containing the functions for the wobbling frequency of a triaxial rotor.
+    """
+    A1: float
+    A2: float
+    A3: float
+    j1: float
+    j2: float
+
+    def __init__(self, moi1: float, moi2: float, moi3: float, odd_spin: float, theta_deg: float) -> None:
+        self.A1 = 1.0/(2.0*moi1)
+        self.A2 = 1.0/(2.0*moi2)
+        self.A3 = 1.0/(2.0*moi3)
+        self.j1 = float(odd_spin*np.cos(theta_deg*np.pi/180.0))
+        self.j2 = float(odd_spin*np.sin(theta_deg*np.pi/180.0))
