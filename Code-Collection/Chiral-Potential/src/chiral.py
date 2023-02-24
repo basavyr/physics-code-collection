499f6d78565c6025e40ddd6ea363c484af3f882a
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-02-24 07:06:47 +0200

    add elliptic potential inside the modulus

--- Code-Collection/Chiral-Potential/src/chiral.py
+++ Code-Collection/Chiral-Potential/src/chiral.py
@@ -1,5 +1,6 @@
-import elliptic
+import elliptic as elliptic_functions
 from dataclasses import dataclass
+import numpy as np
 
 
 @dataclass
@@ -7,19 +8,22 @@ class Potential:
     """
     - Class that calculates the Elliptic potential V(q) and also the Chiral Potential (symmetric and asymmetric terms)
     """
-    sn: float
-    cn: float
-    dn: float
-    k: float
+    elliptic: elliptic_functions.EllipticFunctions
 
-    def __init__(self, k: float, sn: float, cn: float, dn: float) -> None:
-        self.sn = sn
-        self.cn = cn
-        self.dn = dn
-        self.k = k
+    def __init__(self, moi: tuple[float, float, float], odd_spin: float) -> None:
+        moi1, moi2, moi3 = moi
+        self.elliptic = elliptic_functions.EllipticFunctions(
+            moi1, moi2, moi3, odd_spin)
 
-    def v_q(self, q: float, spin: float, theta: float) -> float:
+    def v_q(self, q: float, spin: float, theta_deg: float) -> float:
         """
         - Calculate the potential using the elliptic functions
         """
-        
+        k = self.elliptic.modulus_k(spin, theta_deg)
+        s = self.elliptic.sn(q, k)
+        c = self.elliptic.cn(q, k)
+        d = self.elliptic.dn(q, k)
+        v0 = self.elliptic.v0_factor(spin, theta_deg)
+        return spin*(spin+1.0)*np.power(k, 2)+np.power(v0, 2)+(2.0*spin+1.0)*v0*c*d
+
+    def v_q_chiral(self, q: float, spin: float, theta_deg: float)
--- Code-Collection/Chiral-Potential/src/main.py
+++ Code-Collection/Chiral-Potential/src/main.py
@@ -1,11 +1,16 @@
 import config
 import chiral
+import elliptic
 
 
-def main():
+def fit1() -> None:
     params = config.FittingParameters()
     moi_fit1, odd_spin, theta_deg_fit1 = params.fit1()
-    print(moi_fit1)
+    ellip_func = elliptic.EllipticFunctions(
+        moi_fit1[0], moi_fit1[0], moi_fit1[0], odd_spin)
+
+def main():
+    fit1()
 
 
 if __name__ == '__main__':
