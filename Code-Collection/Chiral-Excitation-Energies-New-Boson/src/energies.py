987499a977504a1be8c3cf54d824c330ab2d6f3a
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-02-26 14:33:12 +0200

    add wobbling frequency

--- Code-Collection/Chiral-Excitation-Energies-New-Boson/src/energies.py
+++ Code-Collection/Chiral-Excitation-Energies-New-Boson/src/energies.py
@@ -14,11 +14,27 @@ class Energies:
     j1: float
     j2: float
 
-    def __init__(self, moi: tuple[float, float, float], odd_spin: float, theta_deg: float) -> None:
+    def __init__(self, moi: tuple[float, float, float], odd_spin: float) -> None:
         self.A1, self.A2, self.A3 = (1.0/(2.0*moi_k) for moi_k in moi)
         self.j = odd_spin
-        self.j1 = odd_spin*np.cos(theta_deg*np.pi/180.0)
-        self.j2 = odd_spin*np.sin(theta_deg*np.pi/180.0)
 
     def show(self):
         print(self.A1, self.A2, self.A3, self.j1, self.j2)
+
+    def a21(self, spin: float, theta_deg: float) -> float:
+        j2 = self.j*np.sin(theta_deg*np.pi/180.0)
+        return self.A2-self.A1-(self.A2*j2)/spin
+
+    def omega(self, spin: float, theta_deg: float) -> float:
+        a1j1 = self.A1*self.j*np.cos(theta_deg*np.pi/180.0)
+        sub_term1 = (2.0*spin+1.0)*self.a21(spin, theta_deg)+2.0*a1j1
+        sub_term2 = (2.0*spin+1.0)*(self.A3-self.A1)+2.0*a1j1
+        sub_term3 = (self.A3-self.A1)*self.a21(spin, theta_deg)
+        return np.sqrt(sub_term1 * sub_term2 - sub_term3)
+
+    def omega_prime(self, spin: float, theta_deg: float) -> float:
+        a1j1 = self.A1*self.j*np.cos(theta_deg*np.pi/180.0)
+        sub_term1 = (2.0*spin+1.0)*self.a21(spin, theta_deg)-2.0*a1j1
+        sub_term2 = (2.0*spin+1.0)*(self.A3-self.A1)-2.0*a1j1
+        sub_term3 = (self.A3-self.A1)*self.a21(spin, theta_deg)
+        return np.sqrt(sub_term1 * sub_term2 - sub_term3)
