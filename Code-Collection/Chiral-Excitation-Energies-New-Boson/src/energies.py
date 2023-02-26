1e821ef0e113c7a092a6500545d646ce16e77615
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-02-26 16:12:56 +0200

    refactor j_vec and the asymmetric energy

--- Code-Collection/Chiral-Excitation-Energies-New-Boson/src/energies.py
+++ Code-Collection/Chiral-Excitation-Energies-New-Boson/src/energies.py
@@ -21,30 +21,43 @@ class Energies:
     def show(self):
         print(self.A1, self.A2, self.A3, self.j1, self.j2)
 
+    def j_vec(self, theta_deg: float, component_k: int) -> float:
+        vec = (
+            self.j*np.cos(theta_deg*np.pi/180.0), self.j*np.sin(theta_deg*np.pi/180.0), 0)
+        return vec[component_k-1]
+
     def a21(self, spin: float, theta_deg: float) -> float:
-        j2 = self.j*np.sin(theta_deg*np.pi/180.0)
-        return self.A2-self.A1-(self.A2*j2)/spin
+        return self.A2-self.A1-(self.A2*self.j_vec(theta_deg, 2))/spin
 
     def omega(self, spin: float, theta_deg: float) -> float:
-        a1j1 = self.A1*self.j*np.cos(theta_deg*np.pi/180.0)
+        a1j1 = self.A1*self.j_vec(theta_deg, 1)
+
         sub_term1 = (2.0*spin+1.0)*self.a21(spin, theta_deg)+2.0*a1j1
         sub_term2 = (2.0*spin+1.0)*(self.A3-self.A1)+2.0*a1j1
         sub_term3 = (self.A3-self.A1)*self.a21(spin, theta_deg)
+
         return np.sqrt(sub_term1 * sub_term2 - sub_term3)
 
     def omega_prime(self, spin: float, theta_deg: float) -> float:
         a1j1 = self.A1*self.j*np.cos(theta_deg*np.pi/180.0)
+
         sub_term1 = (2.0*spin+1.0)*self.a21(spin, theta_deg)-2.0*a1j1
         sub_term2 = (2.0*spin+1.0)*(self.A3-self.A1)-2.0*a1j1
         sub_term3 = (self.A3-self.A1)*self.a21(spin, theta_deg)
+
         return np.sqrt(sub_term1 * sub_term2 - sub_term3)
 
     def symmetric_excitation_energy(self, spin: float, theta_deg: float):
         symmetric_omega = (
             self.omega(spin, theta_deg) + self.omega(spin, theta_deg+180.0))
+
         return self.A1*np.power(spin, 2)+0.5*symmetric_omega
 
     def asymmetric_excitation_energy(self, spin: float, theta_deg: float):
+        a1j1 = self.A1*self.j_vec(theta_deg, 1)
+        a2j2 = self.A2*self.j_vec(theta_deg, 2)
+
         asymmetric_omega = (
             self.omega(spin, theta_deg) - self.omega(spin, theta_deg+180.0))
-        return self.A1*np.power(spin, 2)+0.5*asymmetric_omega
+
+        return (2.0*spin+1.0)*a1j1-spin*a2j2*0.5*asymmetric_omega
